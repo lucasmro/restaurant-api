df164874f7384ef2822b075a1985efdd416bb2d1
Lucas Michelini Reis de Oliveira <lucasmro@gmail.com>
2014-09-01 02:03:20 -0300

    Custom Jongo Mapper and Serialization Group
    - Add Group to filter object attributes on (un)marshalling with Jongo.

--- src/main/java/com/lucasmro/restaurant/dao/MongoDBManager.java
+++ src/main/java/com/lucasmro/restaurant/dao/MongoDBManager.java
@@ -4,7 +4,11 @@ import java.net.UnknownHostException;
 
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
+import org.jongo.marshall.jackson.JacksonMapper;
 
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.lucasmro.restaurant.serialization.Group;
 import com.mongodb.DB;
 import com.mongodb.Mongo;
 
@@ -15,7 +19,11 @@ public class MongoDBManager {
 		try {
 			Mongo mongo = new Mongo("127.0.0.1", 27017);
 			DB db = mongo.getDB("test");
-			jongo = new Jongo(db);
+			jongo = new Jongo(db, new JacksonMapper.Builder()
+					.registerModule(new JodaModule())
+					.enable(MapperFeature.AUTO_DETECT_GETTERS)
+					.withView(Group.Public.class)
+					.build());
 		} catch (UnknownHostException e) {
 		}
 	}
---
+++ src/main/java/com/lucasmro/restaurant/serialization/Group.java
@@ -0,0 +1,6 @@
+package com.lucasmro.restaurant.serialization;
+
+public class Group {
+	public static class Public {}
+	public static class Private extends Public {}
+}
